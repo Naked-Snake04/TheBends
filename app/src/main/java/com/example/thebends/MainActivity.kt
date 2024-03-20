916cc82bd17fe0c3418728ee70fda6ab7b3e853a
Naked-Snake04 <joKer041200saV@gmail.com>
2024-03-20 17:20:32 +0300

    Описание плана дальнейшей работы

--- app/src/main/java/com/example/thebends/MainActivity.kt
+++ app/src/main/java/com/example/thebends/MainActivity.kt
@@ -6,11 +6,21 @@ import com.example.thebends.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//             TODO:
+//              1)Сделать классы
+//                а)Audio, в котором будут записываться звуки
+//                б)Tuner, в котором будет высчитываться частота звука и определяться нота
+//              2)Расчёты вычисления запихнуть в freq в activity_main
+//              3)Запихнуть алгоритм по обработке бэндов в Tuner
+//              4)Добавить в ActivityMain соответствующие кнопки для показа что бэнд взят верно
+//                а также выбор параметров для этого)
+
     }
 }
