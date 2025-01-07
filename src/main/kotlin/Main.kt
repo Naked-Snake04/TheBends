00268f11fa57a2e91d1d9ff08b875b0134dbf4ab
Naked-Snake04 <joKer041200saV@gmail.com>
2025-01-07 10:04:56 +0300

    Первый коммит

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import javax.sound.sampled.*
+import kotlin.math.*
+import java.awt.*
+import javax.swing.*
+
+fun main() {
+    val frame = JFrame("The Bends")
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.setSize(400,200)
+
+    frame.isVisible = true
+}
