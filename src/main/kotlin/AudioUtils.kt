1572daa1657d45a68f49ed6984a5e380a17f80a3
Naked-Snake04 <joKer041200saV@gmail.com>
2025-01-07 10:15:27 +0300

    Добавлена возможность загрузить файлы. Создан класс по анализу файла (ещё в реализации)

---
+++ src/main/kotlin/AudioUtils.kt
@@ -0,0 +1,11 @@
+import java.io.File
+import javax.swing.JLabel
+
+class AudioUtils {
+
+    companion object {
+        fun analyzeAudioFile(file: File?, label: JLabel) {
+        // TODO: Реализовать анализ аудиофайла
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,34 @@
 import javax.sound.sampled.*
 import kotlin.math.*
 import java.awt.*
+import java.io.File
 import javax.swing.*
+import kotlin.concurrent.thread
 
 fun main() {
     val frame = JFrame("The Bends")
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.setSize(400,200)
 
+    val label = JLabel("Загрузите аудио-файл для анализа.", SwingConstants.CENTER)
+    label.font = Font("Arial", Font.PLAIN, 18)
+    frame.add(label, BorderLayout.CENTER)
+
+    val fileChooser = JFileChooser();
+    val loadButton = JButton("Загрузить файл")
+
+    loadButton.addActionListener {
+        val returnValue = fileChooser.showOpenDialog(frame)
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            val file = fileChooser.selectedFile
+            label.text = "Обрабатывается: ${file.name}"
+            thread {
+                AudioUtils.analyzeAudioFile(file, label)
+            }
+        }
+    }
+
+    frame.add(loadButton, BorderLayout.SOUTH)
     frame.isVisible = true
 }
